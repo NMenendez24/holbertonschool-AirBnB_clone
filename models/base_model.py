09050e0a6e8aa35f7082f21f9f32422e976a6a2e
NMenendez24 <nahuelmt24@gmail.com>
2023-02-23 13:54:27 -0300

    Fixed str method and to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -7,17 +7,17 @@ from datetime import datetime
 class BaseModel():
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self) -> str:
-        return f"[{__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.now
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         self.__dict__.update({'__class__': type(self).__name__,
                               'created_at': self.created_at,
-                              'updated_at': self.updated_at})
+                              'updated_at': self.updated_at.isoformat()})
         return self.__dict__
