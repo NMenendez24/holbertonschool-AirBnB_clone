5bf5b2850a6e53d685ca0b0c9f198668c5ee64a7
NMenendez24 <nahuelmt24@gmail.com>
2023-02-23 13:23:28 -0300

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,21 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
-    def __init__(self, created_at, updated_at, id=None):
+    def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self) -> str:
+        return f"[{__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now
+
+    def to_dict(self):
+        self.__dict__.update({'__class__': type(self).__name__,
+                              'created_at': self.created_at,
+                              'updated_at': self.updated_at})
+        return self.__dict__
