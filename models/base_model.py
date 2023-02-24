e6a5a5ba5f6aefdd84ffff933d1eda396e27ad42
NMenendez24 <nahuelmt24@gmail.com>
2023-02-24 18:48:02 -0300

    Some changes

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -16,12 +17,14 @@ class BaseModel():
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self) -> str:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         self.__dict__.update({'__class__': type(self).__name__,
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,26 +3,30 @@ import json
 import os
 
 
-class FileStorage():
+class FileStorage:
     __objects = {}
     __file_path = "file.json"
 
     def all(self):
+        """returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        setattr(self.__objects, f"{obj.__name__}.{obj.id}", obj)
+        setattr(self.__objects, f"{obj.__class__.__name__}.{obj.id}", obj)
+        """This doesn´t work we should make a loop to iterate through the keys"""
 
     def save(self):
         """serializes __objects to the JSON file"""
-        with open(self.__file_path, "w") as f:
-            json.dump(self.__objects, f)
+        objs_dump = {key: value.to_dict()
+                     for key, value in self.__objects.items()}
+        with open(self.__file_path, "w", encoding='utf-8') as fd:
+            json.dump(objs_dump, fd)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as f:
-                self.__objects = json.load(f)
-        else:
-            return
+            with open(self.__file_path, "r", encoding='utf-8') as f:
+                reloaded = json.load(f)
+                for key, value in reloaded.items():
+                    setattr(self.__objects, key, value)
